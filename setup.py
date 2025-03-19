43e2977e86b228b97e3a6dd17e3e19d8e2e43a52
mieraci22 <mieraci22@gmail.com>
2025-03-18 20:09:34 -0400

    uploading test package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/mieraci22/m09-demo2025',
+    author='Michael Ieraci',
+    author_email='_',
+    license='MIT',
+    packages=['demo']
+)
